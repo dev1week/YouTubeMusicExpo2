304b1886745adf48231d5656f83a91bf03306f5e
hanju <the_1_week@naver.com>
2023-09-01 10:22:00 +0900

    #1 -5 카테고리 헤더 선택시 이미지 동적 적용

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { Bottom } from './src/Bottom';
 import { CategoryHeader } from './src/CategoryHeader';
 import { HeaderBackground } from './src/HeaderBackground';
 import { LogoHeader } from './src/LogoHeader';
+import { useState } from 'react';
 
 export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(); 
+
   return (
     <SafeAreaView style={{flex:1, paddingTop:25} }>
         <HeaderBackground/>
         <LogoHeader/>
-        <CategoryHeader/>
+        <CategoryHeader
+          selectedCategory= {selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+          />
         <ScrollView style={{borderWidth: 1}}>
           <View style={{height: 1000}}>
           <Text>list</Text>
--- src/CategoryHeader.js
+++ src/CategoryHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 
-export const CategoryHeader =()=>{
+export const CategoryHeader =({selectedCategory,  setSelectedCategory})=>{
     const category = [
         { id: '1', name: '휴식' },
         { id: '2', name: '에너지 충전' },
@@ -9,6 +9,13 @@ export const CategoryHeader =()=>{
         { id: '4', name: '운동' },
         { id: '5', name: '출퇴근/등하교' },
     ];
+
+
+    const onPressCategory =(index)=>{
+        const data = selectedCategory === index? undefined:index;
+        setSelectedCategory(data); 
+    }
+
 
     return (
         <View>
@@ -21,18 +28,18 @@ export const CategoryHeader =()=>{
                 >
                 {category.map((value)=>{
                     return(
-                        <TouchableOpacity key={value.id} >
+                        <TouchableOpacity key={value.id} onPress={()=>onPressCategory(value.id)} >
                             <View style={{
                                 padding:8, 
                                 paddingHorizontal:16, 
                                 borderWidth:1, 
-                                backgroundColor: '#ffffff10',
+                                backgroundColor: selectedCategory ===value.id ? '#ffffff': '#ffffff10',
                                 borderColor: '#ffffff30',
                                 marginHorizontal: 4, 
                                 borderRadius: 8 
 
                                 }}>
-                                <Text style={{color:'white'}}>{value.name}</Text>
+                                <Text style={{color: selectedCategory ===value.id ?'#111': 'white'}}>{value.name}</Text>
                             </View>
                         </TouchableOpacity>
                     );
