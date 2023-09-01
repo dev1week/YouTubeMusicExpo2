71c1511a91d169436f418791f1c3f5da997bd465
hanju <the_1_week@naver.com>
2023-09-01 10:05:03 +0900

    #1 -4 헤더, 바텀 탭 app에 적용
    렌더링 정상작동 확인

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { Bottom } from './src/Bottom';
+import { CategoryHeader } from './src/CategoryHeader';
+import { HeaderBackground } from './src/HeaderBackground';
+import { LogoHeader } from './src/LogoHeader';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{flex:1, paddingTop:25} }>
+        <HeaderBackground/>
+        <LogoHeader/>
+        <CategoryHeader/>
+        <ScrollView style={{borderWidth: 1}}>
+          <View style={{height: 1000}}>
+          <Text>list</Text>
+        </View>
+        </ScrollView>
+        <Bottom/>
+
+    </SafeAreaView>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
