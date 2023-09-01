a769af90c2ff53b97461f0dce960fbaaa9d324d7
hanju <the_1_week@naver.com>
2023-09-01 12:15:17 +0900

    #1 -8 카테고리 클릭마다 그라데이션 동적 적용

--- src/HeaderBackground.js
+++ src/HeaderBackground.js
@@ -1,16 +1,30 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
+import LinearGradient from "react-native-linear-gradient";
 
-
-export const HeaderBackground = ()=>{
+export const HeaderBackground = (selectedCategory)=>{
+    console.log(selectedCategory.selectedCategory);
+    
     return(
         <View style={{
             position: 'absolute', 
-            height: 300,
+            height: 350,
             width: '100%',
-            backgroundColor: '#222'
+            backgroundColor: '#11111111'
         }}>
-            <Text> Header BG</Text>
+
+            {selectedCategory.selectedCategory===undefined ? (
+                <Image 
+                source={require('../assets/bg1.png')}
+                style={{width:'100%', height: 350, opacity:0.3}}
+                />
+            ):(
+                <Image 
+                source={{uri: `https://picsum.photos/30${selectedCategory.selectedCategory}`}}
+                style={{width:'100%', height: 350, opacity: 0.1 }}
+                />
+            )}
+           
         </View>
     )
 }
