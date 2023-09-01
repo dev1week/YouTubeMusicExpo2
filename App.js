b8d838bfbe275175d1e73be9a99a4cd079f72c26
dev1week <rondo2860@gmail.com>
2023-09-01 22:42:00 +0900

    #2 -6 App.js 적용

--- App.js
+++ App.js
@@ -1,38 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
 
-import { Bottom } from './src/bottom/Bottom';
-import { CategoryHeader } from './src/header/CategoryHeader';
-import { HeaderBackground } from './src/header/HeaderBackground';
-import { LogoHeader } from './src/header/LogoHeader';
-import { useState } from 'react';
+import { Bottom } from "./src/bottom/Bottom";
+import { CategoryHeader } from "./src/header/CategoryHeader";
+import { HeaderBackground } from "./src/header/HeaderBackground";
+import { LogoHeader } from "./src/header/LogoHeader";
+import { useState } from "react";
 
-import {MusicListLarge} from './src/musiclist/MusicListLarge'
-import {MusicListSmall} from './src/musiclist/MusicListSmall'
-import {MusicListMedium} from './src/musiclist/MusicListMedium'
-
+import { MusicListLarge } from "./src/musiclist/MusicListLarge";
+import { MusicListSmall } from "./src/musiclist/MusicListSmall";
+import { MusicListMedium } from "./src/musiclist/MusicListMedium";
 
 export default function App() {
-  const [selectedCategory, setSelectedCategory] = useState(); 
+  const [selectedCategory, setSelectedCategory] = useState();
 
   return (
-    <SafeAreaView style={{flex:1, paddingTop:25, backgroundColor:'#111'} }>
-        <HeaderBackground selectedCategory={selectedCategory}/>
-        <LogoHeader/>
-        <CategoryHeader
-          selectedCategory= {selectedCategory}
-          setSelectedCategory={setSelectedCategory}
-          />
-        <ScrollView style={{borderWidth:1}}>
-          <View style={{height: 1000}}>
-            <MusicListSmall/> 
-            <MusicListMedium/>
-          </View>
-        </ScrollView>
-        <Bottom/>
-
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#111" }}>
+      <HeaderBackground selectedCategory={selectedCategory} />
+      <LogoHeader />
+      <CategoryHeader
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+      />
+      <ScrollView>
+        <View style={{ marginBottom: 100 }}>
+          <MusicListSmall />
+          <MusicListMedium />
+          <MusicListLarge />
+        </View>
+      </ScrollView>
+      <Bottom />
     </SafeAreaView>
-   
   );
 }
 
